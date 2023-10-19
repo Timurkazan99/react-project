9916e43fcadcc5b5799d456f2a83983ac788da4a
timurkazan99 <timurkazan99@gmail.com>
2023-10-19 17:38:40 +0300

    add types

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,12 +1,39 @@
-export interface Countries {
-    //добавить поля
+export interface Image {
+  png: string,
+  alt: string,
 }
 
-export interface User {
- //добавить поля
+export interface Country {
+  cca3: string,
+  name: {
+    common: string,
+    official: string
+  },
+  flags: Image,
+  coatOfArms: Image,
+  capital: string[],
+  population: number
+}
+
+export interface FullCountry extends Country {
+  independent: boolean,
+  area: number,
+  continents: string[],
+  currencies: {
+    [key: string]: {
+      name: string,
+      symbol: string
+    }
+  },
+  languages: {
+    [key: string]: string
+  },
+  region: string,
+  subregion: string
 }
 
 export interface History {
-
- //добавить поля
+  type: string,
+  search: string,
+  timestamp: number
 }
