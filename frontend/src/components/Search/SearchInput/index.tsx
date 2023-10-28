0942e290141e0e1450f3c5167e6845e9f8deb6b3
timurkazan99 <timurkazan99@gmail.com>
2023-10-28 18:18:45 +0300

    change logic

--- frontend/src/components/Search/SearchInput/index.tsx
+++ frontend/src/components/Search/SearchInput/index.tsx
@@ -1,30 +1,15 @@
 import { ChangeEvent } from 'react';
-import useDebounce from '../../../hooks/useDebounce';
 import { useSearchContext } from '../SearchContext';
-import { getSearchedCountries } from '../../../API';
-import { INPUT_DELAY } from '../../../utils';
 
 function SearchInput() {
   const {
-    search, type, setSearch, setResults,
+    search, setSearch, setResults,
   } = useSearchContext();
-
-  const getResults = useDebounce(async (bindSearch) => {
-    if (bindSearch) {
-      try {
-        const data = await getSearchedCountries(type, bindSearch);
-        setResults(data.slice(0, 4));
-      } catch (e) {
-        setResults([]);
-      }
-    }
-  }, INPUT_DELAY);
 
   const inputHandler = async (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setResults([]);
     setSearch(value);
-    getResults(value);
   };
 
   return (
