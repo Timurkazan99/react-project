8ed387042b70d45663f8cdf492b204b7955e6824
timurkazan99 <timurkazan99@gmail.com>
2023-10-29 18:42:34 +0300

    add updateHistory

---
+++ frontend/src/hooks/useUpdateHistory.ts
@@ -0,0 +1,26 @@
+import {
+  addHistory, getAllHistory, getUser, useAppDispatch, useAppSelector,
+} from '../store';
+import useGetAuth from './useGetAuth';
+
+function useUpdateHistory() {
+  const dispatch = useAppDispatch();
+  const history = useAppSelector(getAllHistory);
+  const { login } = useAppSelector(getUser);
+  const auth = useGetAuth();
+
+  return (search: string, type: string) => {
+    if (auth) {
+      const newValue = {
+        type,
+        search,
+        timestamp: Date.now(),
+      };
+      dispatch(addHistory(newValue));
+
+      localStorage.setItem(`${login}_history`, JSON.stringify([...history, newValue]));
+    }
+  };
+}
+
+export default useUpdateHistory;
