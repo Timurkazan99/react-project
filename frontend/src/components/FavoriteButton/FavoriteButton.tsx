23dc0591b6fab664a4d8c89a549e2a0928b43152
DenisLatyshau <denislatyshau@yandex.ru>
2023-10-29 22:03:34 +0300

    feat: FavoriteButton.tsx

--- frontend/src/components/FavoriteButton/FavoriteButton.tsx
+++ frontend/src/components/FavoriteButton/FavoriteButton.tsx
@@ -1,9 +1,14 @@
+import { addFavoriteCountry, getUser, useAppSelector, deleteFavoriteCountry, useAppDispatch } from '../../store';
 import './FavoriteButton.scss'
 
 function FavoriteButton({ id }) {
+  const { login } = useAppSelector(getUser)
+
+  const dispatch = useAppDispatch()
 
   const toggleFavorite = () => {
     console.log(id);
+    localStorage.setItem(`${login}_favorite`, addFavoriteCountry())
   };
 
   return (
